2a9bf460364504e7e5ffdde85baddc9cbcbefa32
Duy Nguyen <duy.hoa@ascendcorp.com>
2016-11-05 17:20:42 +0700

    Fixing wrong interface defined

--- src/Channels/FirebaseChannel.php
+++ src/Channels/FirebaseChannel.php
@@ -5,7 +5,7 @@ namespace DouglasResende\FCM\Channels;
 use DouglasResende\FCM\Messages\FirebaseMessage;
 use Illuminate\Contracts\Config\Repository as Config;
 use GuzzleHttp\Client;
-use DouglasResende\FCM\FirebaseNotification as Notification;
+use DouglasResende\FCM\Contracts\FirebaseNotification as Notification;
 
 /**
  * Class FirebaseChannel
